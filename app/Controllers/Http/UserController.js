82f216ef0d0e4423e7063ce99d5b858aea3576b8
Emeka <emekaosuagwuandela@gmail.com>
2018-12-01 04:04:09 +0100

    add upload functionality

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,10 +1,22 @@
 'use strict'
 
+/*
+|--------------------------------------------------------------------------
+| Services Namespaces
+|--------------------------------------------------------------------------
+*/
+const CloudineryService = use("App/Services/CloudineryService");
+
 class UserController {
-	
+
 	async index({request, response}){
+
+		const file = request.file('file')
+
+		const userService = new CloudineryService('arm', '169264285654842', 'M_8S1mNBX_Q4s8KCUrCJDk4wI0g');
+
 		return response.send({
-			data: 200
+			data: await userService.updateAsset(file.tmpPath)
 		})
 	}
 
--- app/Services/CloudineryService.js
+++ app/Services/CloudineryService.js
@@ -6,13 +6,15 @@
 const Env = use("Env");
 const cloudinary = require('cloudinary');
 
-cloudinary.config({
-	cloud_name: Env.get('CLOUDINARY_CLOUD_NAME'),
-	api_key: Env.get('CLOUDINARY_API_KEY'),
-	api_secret: Env.get('CLOUDINARY_API_SECRET')
-});
-
 class CloudineryService {
+
+	constructor(CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET) {
+		cloudinary.config({
+			cloud_name: CLOUDINARY_CLOUD_NAME,
+			api_key: CLOUDINARY_API_KEY,
+			api_secret: CLOUDINARY_API_SECRET
+		});	
+	}
 
 	async updateAsset(data) {
 		return await cloudinary.v2.uploader.upload(data, {folder: 'uniclip'});

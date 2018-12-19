17dbd687e62e3837b7f14c065b3078f2784ddcfe
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2018-12-19 09:32:59 +0100

    update

--- app/Controllers/Http/MailController.js
+++ app/Controllers/Http/MailController.js
@@ -5,6 +5,7 @@
 |--------------------------------------------------------------------------
 */
 const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 const CloudineryService = use("App/Services/CloudineryService");
 const CloudineryValidation = use("App/Validations/CloudineryValidation");
 
@@ -17,42 +18,6 @@ const cloudineryService = new CloudineryService();
 const cloudineryValidation = new CloudineryValidation();
 
 class MailController {
-
-    async sendOther({request, response}){
-
-        nodemailer.createTestAccount((err, account) => {
-
-            // create reusable transporter object using the default SMTP transport
-            let transporter = nodemailer.createTransport({
-                host: request.header('MAIL_HOST'),
-                port: request.header('MAIL_PORT'),
-                secure: false,
-                auth: {
-                    user: request.header('MAIL_USERNAME'),
-                    pass: request.header('MAIL_PASSWORD')
-                }
-            });
-
-            // setup email data with unicode symbols
-            let mailOptions = {
-                from: request.input('sender'), // sender address
-                to: request.input('receiver'), // list of receivers
-                subject: request.input('subject'), // Subject line
-                // text: 'Hello world?', // plain text body
-                html: request.input('html') // html body
-            };
-
-            // send mail with defined transport object
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    return console.log(error);
-                }
-                console.log('Message sent: %s', info.messageId);
-                // Preview only available when sending through an Ethereal account
-                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-            });
-        });
-    }
 
     async send({request, response}){
 
@@ -89,6 +54,17 @@ class MailController {
             });
         });
     }
+
+    // async send({request, response}){
+    //     sgMail.setApiKey("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");
+    //     const msg = {
+    //         to: request.input('receiver'),
+    //         from: request.input('sender'),
+    //         subject: request.input('subject'),
+    //         html: request.input('html'),
+    //     };
+    //     sgMail.send(msg);
+    // }
 }
 
 module.exports = MailController
